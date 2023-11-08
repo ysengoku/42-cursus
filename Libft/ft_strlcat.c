2f288045742fd1f71284f781967a9768da3cf0ce
ysengoku <ysengoku.tourisme@gmail.com>
2023-11-08 23:13:04 +0100

    update 2 files

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -20,17 +20,18 @@ size_t  ft_strlcat(char *dst, const char *src, size_t size)
 
     dst_len = ft_strlen(dst);
     src_len = ft_strlen((char*)src);
+
     if (dst_len >= size)
         return (size + src_len);
     i = 0;
-    while(!src[i] || i < size - dst_len - 1)
+    while(src[i] && dst_len + i < size - 1)
     {
         dst[dst_len + i] = src[i];
         i++;
     }
+    dst[dst_len + i] = '\0';
     return (dst_len + src_len);
 }
-
 /*
 #include <stdio.h>
 #include <string.h>
