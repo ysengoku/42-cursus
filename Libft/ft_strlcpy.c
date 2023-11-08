5b4c88116317e1841cde21632fbd9b33465fe5f0
yusengok <yusengok@student.42.fr>
2023-11-08 09:49:38 +0100

    update 3 files

--- Libft/ft_strlcpy.c
+++ Libft/ft_strlcpy.c
@@ -6,13 +6,22 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 16:34:50 by yusengok          #+#    #+#             */
-/*   Updated: 2023/11/07 16:49:39 by yusengok         ###   ########.fr       */
+/*   Updated: 2023/11/08 09:39:02 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    ft_strlcpy(char *dst, const char *src, size_t size)
+size_t    ft_strlcpy(char *dst, const char *src, size_t size)
 {
-    
+    unsigned int    i;
+
+    i = 0;
+    while(i < sizeof(src) || i < size)
+    {
+        dst[i] = src[i];
+        i++;
+    }
+    dst[i] = '\0';
+    return (sizeof(src));
 }
