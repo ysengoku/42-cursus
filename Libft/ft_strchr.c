4c2ab531f871cdbb3be241fdb81270935064a377
yusengok <yusengok@student.42.fr>
2023-11-09 14:34:51 +0100

    Update 2 files & libft.h

---
+++ Libft/ft_strchr.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/09 13:38:38 by yusengok          #+#    #+#             */
+/*   Updated: 2023/11/09 14:19:47 by yusengok         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return ((char *)&s[i]);
+		else
+			i++;
+	}
+	return (0);
+}
+/*
+#include <string.h>
+#include <stdio.h>
+
+int	main()
+{
+	const char	*str = "Coucou c'est moi.";
+	const char	ch = 'o';
+	
+	printf("--- Original version ---\n");
+	printf("String: %s\nChar to find: %c\n", str, ch);
+	printf("Output: %s\n", strchr(str, ch));
+
+	printf("--- My version ---\n");
+	printf("String: %s\nChar to find: %c\n", str, ch);
+	printf("Output: %s\n", ft_strchr(str, ch));
+	
+	return 0;
+}
+*/
