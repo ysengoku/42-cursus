ed79d6d1663595b7c07833c1a1b12e8e9d8eab2c
yusengok <ysengoku.tourisme@gmail.com>
2023-11-10 16:05:08 +0100

    update 2 files

--- Libft/ft_strjoin.c
+++ Libft/ft_strjoin.c
@@ -6,7 +6,18 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 15:49:53 by yusengok          #+#    #+#             */
-/*   Updated: 2023/11/10 15:49:54 by yusengok         ###   ########.fr       */
+/*   Updated: 2023/11/10 16:04:28 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*res;
+
+	res = (char *)malloc((ft_strlen(s1) + ft_strlen(s2)) * sizeof(char) + 1);
+	if (!res)
+		return (NULL);
+	
+}
--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -6,7 +6,26 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 15:49:32 by yusengok          #+#    #+#             */
-/*   Updated: 2023/11/10 15:49:33 by yusengok         ###   ########.fr       */
+/*   Updated: 2023/11/10 15:58:28 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	i;
+	
+	substr = (char *)malloc(len * sizeof(char));
+	if (!substr)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		substr[i] = s[start];
+		i++;
+		start++;
+	}
+	return (substr);
+}
