3a31b7257a94f5ece97d8198b3509e4fbb712865
yusengok <ysengoku.tourisme@gmail.com>
2023-11-10 14:47:04 +0100

    update ft_calloc

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -6,7 +6,30 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 13:20:20 by yusengok          #+#    #+#             */
-/*   Updated: 2023/11/10 13:20:21 by yusengok         ###   ########.fr       */
+/*   Updated: 2023/11/10 14:45:28 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+nmemb --> number of blocks to be allocated
+size --> size of each block in bytes
+*/
+#include "libft.h"
+#include <stdlib.h>
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	unsigned char	*ptr;
+	size_t			i;
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	while (i < nmemb)
+	{
+		ptr[i] = 0;
+		i++;
+	}
+	return (ptr);
+}
