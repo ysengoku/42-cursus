dd63db452f3a4fa6a123c7c437c16eadd3481937
ysengoku <ysengoku.tourisme@gmail.com>
2023-11-12 21:37:43 +0100

    Update 2 files

--- Libft/libft.h
+++ Libft/libft.h
@@ -43,7 +43,7 @@ int		ft_toupper(int c);
 
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
-
+char	*ft_strtrim(char const *s1, char const *set);
 
 
 
