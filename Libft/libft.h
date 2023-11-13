ea9568d01435d508417376e21ad61b3b6264d585
yusengok <ysengoku.tourisme@gmail.com>
2023-11-13 15:36:35 +0100

    update ft_itoa & add Makefile

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 14:23:54 by yusengok          #+#    #+#             */
-/*   Updated: 2023/11/13 12:40:46 by yusengok         ###   ########.fr       */
+/*   Updated: 2023/11/13 15:06:16 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
-
+char	*ft_itoa(int n);
 
 
 void	ft_putchar_fd(char c, int fd);
