ab927440ca346fdc2aa6ec8d740603cfeba098d5
ysengoku <ysengoku.tourisme@gmail.com>
2023-11-14 22:34:28 +0100

    update 2 files

--- Libft/ft_putnbr_fd.c
+++ Libft/ft_putnbr_fd.c
@@ -14,21 +14,29 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n == -2147483648)
-	{
-		ft_putstr_fd("-2147483648", fd);
-		return ;
-	}
-	if (n >= 0 && n <= 9)
-		ft_putchar_fd(n + '0', fd);
-	else if (n < 9)
-	{
-		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n % 10, fd);
-	}
-	else
+	long	nbr;
+
+	nbr = n;
+	if (nbr < 0)
 	{
 		ft_putchar_fd('-', fd);
-		ft_putnbr_fd(n * (-1), fd);
+		nbr *= -1;
 	}
+	if (nbr > 9)
+	{
+		ft_putnbr_fd(nbr / 10, fd);
+		ft_putchar_fd(nbr % 10 + '0', fd);
+	}
+	else
+		ft_putchar_fd(nbr + '0', fd);
 }
+/*
+int	main(int argc, char **argv)
+{
+	if (argc != 3)
+		return 0;
+	ft_putnbr_fd(atoi(argv[1]), atoi(argv[2]));
+	printf("%c", '\n');
+	return 0;
+}
+*/
