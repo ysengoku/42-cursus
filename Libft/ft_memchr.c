6d341488e2c83959b024ee903015542cf17975ab
yusengok <ysengoku.tourisme@gmail.com>
2023-11-15 09:06:33 +0100

    update ft_memchr

--- Libft/ft_memchr.c
+++ Libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 13:20:13 by yusengok          #+#    #+#             */
-/*   Updated: 2023/11/14 14:23:29 by yusengok         ###   ########.fr       */
+/*   Updated: 2023/11/15 09:03:41 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,15 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char	*str;
-	unsigned char	ch;
+	unsigned char	chr;
 	size_t			i;
 
 	str = (unsigned char *)s;
-	ch = (unsigned char)c;
+	chr = (unsigned char)c;
 	i = 0;
-	while (str[i] && i < n)
+	while (i < n)
 	{
-		if (str[i] == ch)
+		if (str[i] == chr)
 			return ((void *)&str[i]);
 		i++;
 	}
@@ -34,11 +34,18 @@ void	*ft_memchr(const void *s, int c, size_t n)
 
 int	main()
 {
-	int	arr[] = {-49, 49, 1, -1, 0, -2, 2};
-	int	to_find = -1;
-	size_t	n = 7;
+//	int	arr[] = {-49, 49, 1, -1, 0, -2, 2};
+//	int	to_find = -1;
+//	size_t	n = 7;
 
-	printf("Str:  --- to_find: %c\n", to_find);
+	char	arr[] = "bonjourno";
+	int	to_find = 'n';
+	size_t	n = 2;
+
+//	char	arr[] =  {0, 1, 2 ,3 ,4 ,5};
+//	int	to_find = 2 + 256;
+//	size_t	n = 3;
+
 	printf("Original version: %s\n", (char *)memchr(arr, to_find, n));
 	printf("My version: %s\n", (char *)ft_memchr(arr, to_find, n));
 	return 0;
