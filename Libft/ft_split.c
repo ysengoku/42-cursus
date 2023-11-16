03d54ccfd6fcf7ebd7e10fad67f603285c999ff2
yusengok <yusengok@student.42.fr>
2023-11-16 09:51:20 +0100

    update

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,51 +6,34 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 15:50:03 by yusengok          #+#    #+#             */
-/*   Updated: 2023/11/15 15:22:38 by yusengok         ###   ########.fr       */
+/*   Updated: 2023/11/16 09:49:07 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_get_wordcount(char const *s, char c);
-static char	*ft_copy_word(char const *src, char c, int start);
-static void	ft_freeall(char **str, int i);
+static size_t	ft_get_wordcount(char const *s, char c);
+static size_t	ft_get_wordlen(char const *s, size_t start, char c);
+static void		ft_freeall(char **str, size_t i);
+static char		**ft_cpy_words(char const *s, char c, size_t count, char **arr);
 
 char	**ft_split(char const *s, char c)
 {
-	char	**array;
-	int		wordcount;
-	int		i;
-	int		word_start;
+	char	**arr;
+	size_t	wordcount;
 
 	wordcount = ft_get_wordcount(s, c);
-	array = (char **)malloc((wordcount + 1) * sizeof(char *));
-	if (!array)
+	arr = (char **)malloc((wordcount + 1) * sizeof(char *));
+	if (!arr || !s)
 		return (NULL);
-	i = 0;
-	word_start = 0;
-	while (i < wordcount)
-	{
-		while (s[word_start] == c)
-          word_start++;
-		array[i++] = ft_copy_word(s, c, word_start);
-		if (!array[i - 1])
-		{
-			ft_freeall(array, i);
-			return (NULL);
-		}
-		word_start += 1;
-		while (s[word_start - 1] != c && s[word_start])
-			word_start++;
-	}
-	array[i] = NULL;
-	return (array);
+	ft_cpy_words(s, c, wordcount, arr);
+	return (arr);
 }
 
-static int	ft_get_wordcount(char const *s, char c)
+static size_t	ft_get_wordcount(char const *s, char c)
 {
-	int	i;
-	int	count;
+	size_t	i;
+	size_t	count;
 
 	i = 0;
 	while (s[i] == c)
@@ -70,59 +53,69 @@ static int	ft_get_wordcount(char const *s, char c)
 	return (count);
 }
 
-static char	*ft_copy_word(char const *src, char c, int start)
+static size_t	ft_get_wordlen(char const *s, size_t start, char c)
 {
-	int		i;
-	int		len;
-	char	*word;
-	
+	size_t	len;
+
 	len = 0;
-	while (src[start + len] != c && src[start + len])
-		len++;
-	word = (char *)malloc((len + 1) * sizeof(char));
-	if (!word)
-		return (NULL);
-	i = 0;
-	while (i < len)
+	while (s[start] != c)
 	{
-		word[i++] = src[start++];
+		start++;
+		len++;
 	}
-	word[i] = '\0';
-	return (word);
+	return (len);
 }
 
-static void	ft_freeall(char **str, int i)
+static void	ft_freeall(char **str, size_t i)
 {
 	while (i > 0)
 		free(str[i--]);
 	free(str);
 }
 
+static char	**ft_cpy_words(char const *s, char c, size_t count, char **arr)
+{
+	size_t	i;
+	size_t	start;
+	size_t	len;
+	char	**array;
+
+	i = 0;
+	start = 0;
+	len = 0;
+	array = arr;
+	while (i < count)
+	{
+		while (s[start] == c)
+			start++;
+		len = ft_get_wordlen(s, start, c);
+		array[i++] = ft_substr(s, start, len);
+		if (!array[i - 1])
+		{
+			ft_freeall(array, i);
+			return (NULL);
+		}
+		start += len;
+	}
+	arr[i] = NULL;
+	return (array);
+}
+
 int    main()
 {
   char **array;
   int        i = 0;
 
-    array = ft_split("  tripouille  42  ", ' ');
+    array = ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.", 'i');
     while (array[i])
     {
         printf("%s\n", array[i++]);
     }
     return 0;
 }
+
 /*
-int    main()
-{
-  char **array;
-  int        i = 0;
-
     array = ft_split("--1-2--3---4----5-----42", '-');
-    while (array[i])
-    {
-        printf("%s\n", array[i++]);
-    }
-    return 0;
-}
 */
 /*
 int	main(int argc, char **argv)
