dbca71dad9790b1e8a9680d121ea054114950cee
ysengoku <ysengoku.tourisme@gmail.com>
2023-11-16 22:38:16 +0100

    update libft.h & makefile

--- Libft/libft.h
+++ Libft/libft.h
@@ -19,6 +19,12 @@
 # include <stdint.h>
 # include <limits.h>
 
+typedef struct	s_list
+{
+	void			*content;
+	struct s_list	*next; 
+}					t_list;
+
 int		ft_atoi(const char *nptr);
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
@@ -54,5 +60,9 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
 
 #endif
