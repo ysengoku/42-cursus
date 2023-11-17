67236da76b9ad02735bc06abc248bb50297d6a22
yusengok <ysengoku.tourisme@gmail.com>
2023-11-17 16:05:24 +0100

    update ft_lstmap

--- Libft/ft_lstmap.c
+++ Libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 12:13:50 by yusengok          #+#    #+#             */
-/*   Updated: 2023/11/17 14:18:57 by yusengok         ###   ########.fr       */
+/*   Updated: 2023/11/17 16:04:14 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*newlst;
 	t_list	*tmp;
 
+	if (!lst || !f || !des)
+		return (NULL);
 	tmp = lst;
 	newlst = ft_lstnew(f(tmp->content));
 	while (tmp)
