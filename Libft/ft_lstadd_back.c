4ecccf5be65f99a3feae06845e40acb0063d1b30
yusengok <yusengok@student.42.fr>
2023-11-17 11:10:28 +0100

    update 3 files & libft.h, makefile

--- Libft/ft_lstadd_back.c
+++ Libft/ft_lstadd_back.c
@@ -1,25 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/17 08:32:56 by yusengok          #+#    #+#             */
-/*   Updated: 2023/11/17 10:37:49 by yusengok         ###   ########.fr       */
+/*   Created: 2023/11/17 10:32:24 by yusengok          #+#    #+#             */
+/*   Updated: 2023/11/17 10:39:56 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstnew(void *content)
+void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list	newnode;
-
-	newnode = (t_list)malloc(sizeof(t_list));
-	if (!newnode)
-		return (NULL);
-	newnode->content = content;
-	newnode->next = NULL;
-	return (newnode);
+	size_t	tmp;
+	
+	tmp = lst;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	tmp = new;
+	new->next = NULL;
 }
