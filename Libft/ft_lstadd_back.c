1875e9cfd9370c3c9482ed90de5d300ee3be2325
yusengok <ysengoku.tourisme@gmail.com>
2023-11-20 08:06:13 +0100

    fix norm

--- Libft/ft_lstadd_back.c
+++ Libft/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 10:32:24 by yusengok          #+#    #+#             */
-/*   Updated: 2023/11/17 15:56:52 by yusengok         ###   ########.fr       */
+/*   Updated: 2023/11/20 08:01:57 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last;
-	
+
 	last = ft_lstlast(*lst);
 	if (!last)
 		(*lst) = new;
