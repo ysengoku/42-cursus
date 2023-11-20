b7d14137846709d2abc77c66663c9ce4b8c78da4
yusengok <yusengok@student.42.fr>
2023-11-20 09:48:31 +0100

    update lstmap

--- Libft/ft_lstmap.c
+++ Libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 12:13:50 by yusengok          #+#    #+#             */
-/*   Updated: 2023/11/20 09:08:39 by yusengok         ###   ########.fr       */
+/*   Updated: 2023/11/20 09:44:40 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,39 +22,30 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	newlst = NULL;
 	while (lst)
 	{
-		node = ft_lstnew(f(lst->content));
+		node = (t_list *)malloc(sizeof(t_list));
 		if (!node)
 		{
 			ft_lstclear(&newlst, del);
 			return (NULL);
 		}
+		node->content = f(lst->content);
+		node->next = NULL;
 		ft_lstadd_back(&newlst, node);
 		lst = lst->next;
 	}
 	return (newlst);
 }
 
-/*int	main()
+/*
+t_list	*ft_lstnew(void *content)
 {
-	t_list	list;
-	t_list	node1;
-	t_list	node2;
-	t_list	node3;
-	char	*s1 = ft_strdup("abc");
-	char	*s2 = ft_strdup("cdef");
-	char	*s3 = ft_strdup("ghijk");
-	
-	node1 = ft_lstnew(s1);
-	node2 = ft_lstnew(s2);
-	node3 = ft_lstnew(s3);
-	node1->next = node2;
-	node2->next = node3;
-	node3->next = NULL;
-	list = *node1;
+	t_list	*newnode;
 
-	while(list)
-	{
-		
-	}
-	ft_lstmap()
-}*/
+	newnode = (t_list *)malloc(sizeof(t_list));
+	if (!newnode)
+		return (NULL);
+	newnode->content = content;
+	newnode->next = NULL;
+	return (newnode);
+}
+*/
