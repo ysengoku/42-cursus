f4ac1a00120cf4235175d35dfd30fdc84fcea574
yusengok <ysengoku.tourisme@gmail.com>
2023-11-20 09:09:12 +0100

    update 3 files

--- Libft/ft_lstclear.c
+++ Libft/ft_lstclear.c
@@ -6,13 +6,13 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 12:04:08 by yusengok          #+#    #+#             */
-/*   Updated: 2023/11/17 13:39:15 by yusengok         ###   ########.fr       */
+/*   Updated: 2023/11/20 08:57:06 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstclear(t_list **lst, void (*del)(void	*))
+void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*tmp;
 
--- Libft/ft_lstmap.c
+++ Libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 12:13:50 by yusengok          #+#    #+#             */
-/*   Updated: 2023/11/20 08:01:41 by yusengok         ###   ########.fr       */
+/*   Updated: 2023/11/20 09:08:39 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,46 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*newlst;
-	t_list	*newnode;
+	t_list	*node;
 
 	if (!lst || !f || !del)
 		return (NULL);
 	newlst = NULL;
 	while (lst)
 	{
-		newnode = ft_lstnew(f(lst->content));
-		if (!newnode)
+		node = ft_lstnew(f(lst->content));
+		if (!node)
 		{
 			ft_lstclear(&newlst, del);
 			return (NULL);
 		}
-		ft_lstadd_back(&newlst, newnode);
+		ft_lstadd_back(&newlst, node);
 		lst = lst->next;
 	}
 	return (newlst);
 }
+
+/*int	main()
+{
+	t_list	list;
+	t_list	node1;
+	t_list	node2;
+	t_list	node3;
+	char	*s1 = ft_strdup("abc");
+	char	*s2 = ft_strdup("cdef");
+	char	*s3 = ft_strdup("ghijk");
+	
+	node1 = ft_lstnew(s1);
+	node2 = ft_lstnew(s2);
+	node3 = ft_lstnew(s3);
+	node1->next = node2;
+	node2->next = node3;
+	node3->next = NULL;
+	list = *node1;
+
+	while(list)
+	{
+		
+	}
+	ft_lstmap()
+}*/
