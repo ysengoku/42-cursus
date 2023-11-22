741e72c9f139d6d5c36caa924d14532c35837482
yusengok <yusengok@student.42.fr>
2023-11-22 15:39:14 +0100

    add 2 files & update ft_printf.c

---
+++ ft_printf/ft_print_lowerhex.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_lowerhex.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/22 15:34:50 by yusengok          #+#    #+#             */
+/*   Updated: 2023/11/22 15:38:18 by yusengok         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static char	*ft_itoa_base_hexlower(int n);
+
+int	ft_print_lowerhex(int n)
+{
+	char	*base;
+	char	*nbr;
+
+	base = "0123456789abcdef";
+//	nbr = ft_itoa_base_hexlower(n);
+}
+
+static char	*ft_itoa_base_hexlower(int n)
+{
+	
+}
