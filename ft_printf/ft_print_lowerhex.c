020e37474e5ab489fd426eed83d4b2466f8cc3f1
ysengoku <ysengoku.tourisme@gmail.com>
2023-11-22 22:02:18 +0100

    update 3 files

--- ft_printf/ft_print_lowerhex.c
+++ ft_printf/ft_print_lowerhex.c
@@ -12,18 +12,61 @@
 
 #include "ft_printf.h"
 
-static char	*ft_itoa_base_hexlower(int n);
+static int	ft_get_digitcount(int n);
+static char	*ft_itoa_base_hexl(int n);
 
 int	ft_print_lowerhex(int n)
 {
-	char	*base;
 	char	*nbr;
 
-	base = "0123456789abcdef";
-//	nbr = ft_itoa_base_hexlower(n);
+	if (n >= 0 && n <= 9)
+	{
+		ft_putchar_fd(n + '0', 1);
+		return (1);
+	}
+	nbr = ft_itoa_base_hexl(n);
+	ft_putstr_fd(nbr, 1);
+	return (ft_strlen(nbr));
 }
 
-static char	*ft_itoa_base_hexlower(int n)
+static int	ft_get_digitcount(int n)
 {
+	unsigned int	i;
+
+	if (n > 9)
+	{
+		i = 0;
+		while(n > 0)
+		{
+			n /= 16;
+			i++; 
+		}
+	}
+	else
+	{
+
+	}
+	return (i);
+}
+
+static char	*ft_itoa_base_hexl(int n)
+{
+	char	*base;
+	char	*nbr;
+	int		digitcount;
+
+	base = "0123456789abcdef";
+	digitcount = ft_get_digitcount(n);
 	
+	nbr = (char *)malloc((digitcount + 1) * sizeof(char));
+	nbr[digitcount + 1] = '\0';
+	if (n > 0)
+	{
+		while (n > 0)
+		{
+			nbr[digitcount--] = base[n % 16];
+			n /= 16;  
+		}
+	}
+	return (nbr);
 }
--- ft_printf/test.c
+++ ft_printf/test.c
@@ -4,95 +4,101 @@ int	main()
 {
 //	printf(" (%d)\n", ft_printf("hello"));
 
-// ------ TEST1 ------
+// ------ TEST1 / %c ------
 	printf("test1 - printf: ");
 	printf(" (%i)\n", printf("%c", 'a'));
 //	printf("test1 - ft_printf: ");
 //	printf(" (%i)\n", ft_printf("%c", 'a'));
 
-// ------ TEST2 ------
+// ------ TEST2 / %s ------
 	printf("test2 - printf: ");
 	printf(" (%i)\n", printf("%s", "hello"));
 //	printf("test2 - ft_printf: ");
 //	printf(" (%i)\n", ft_printf("%s\n", "hello"));
 
-// ------ TEST3 ------
+// ------ TEST3 / %p ------
 	const char	*test3 = "hello";
 	printf("test3 - printf: ");
 	printf(" (%i)\n", printf("%p", test3));
 //	printf("test3 - ft_printf: ");
 //	printf(" (%i)\n", ft_printf("%p", test3));
 
-// ------ TEST4 ------
+// ------ TEST4 / %d ------
 	printf("test4 - printf: ");
 	printf(" (%i)\n", printf("%d", -42));
 //	printf("test4 - ft_printf: ");
 //	printf(" (%i)\n", ft_printf("%d", -42));
 
-// ------ TEST5 ------
+// ------ TEST5 / %i ------
 	printf("test5 - printf: ");
 	printf(" (%i)\n", printf("%i", 0));
 //	printf("test5 - ft_printf: ");
 //	printf(" (%i)\n", ft_printf("%i", 0));
 
-// ------ TEST6 ------
+// ------ TEST6 / %d ------
 	printf("test6 - printf: ");
 	printf(" (%i)\n", printf("%d", 2147483647));
 //	printf("test6 - ft_printf: ");
 //	printf(" (%i)\n", ft_printf("%d", 2147483647);
 
-// ------ TEST7 ------
+// ------ TEST7 / %u ------
 	printf("test7 - printf: ");
 	printf(" (%i)\n", printf("%u", 42));
 //	printf("test7 - ft_printf: ");
 //	printf(" (%i)\n", ft_printf("%u", 42));
 
-// ------ TEST8 ------
+// ------ TEST8 / %x ------
 	printf("test8 - printf: ");
 	printf(" (%i)\n", printf("%x", 1011));
 //	printf("test8 - ft_printf: ");
 //	printf(" (%i)\n",ft_printf("%x", 1011);
 
-// ------ TEST9 ------
+// ------ TEST9 / %X ------
 	printf("test9 - printf: ");
-	printf(" (%i)\n", printf("%X", 1011));
+	printf(" (%i)\n", printf("%X", 9));
 //	printf("test9 - ft_printf: ");
-//	printf(" (%i)\n",ft_printf("%X", 1011));
+//	printf(" (%i)\n",ft_printf("%X", 9));
 
-// ------ TEST10 ------
+// ------ TEST10 / %X ------
 	printf("test10 - printf: ");
-	printf(" (%i)\n", printf("%%"));
+	printf(" (%i)\n", printf("%X", -365));
 //	printf("test10 - ft_printf: ");
+//	printf(" (%i)\n",ft_printf("%X", -365));
+
+// ------ TEST11 / %% ------
+	printf("test11 - printf: ");
+	printf(" (%i)\n", printf("%%"));
+//	printf("test11 - ft_printf: ");
 //	printf(" (%i)\n",ft_printf("%%"));
 
-// ------ TEST11 ------
-	printf("test11 - printf: ");
+// ------ TEST12 / %s %d ------
+	printf("test12 - printf: ");
 	printf(" (%i)\n", printf("I'm %s, %d years old.", "Coco", 18));
-//	printf("test11 - ft_printf: ");
+//	printf("test12 - ft_printf: ");
 //	printf(" (%i)\n",ft_printf("I'm %s, %d years old", "Coco", 18));
 
-// ------ TEST12 ------
-	printf("test12 - printf: ");
+// ------ TEST13 / nil ------
+	printf("test13 - printf: ");
 	printf(" (%i)\n", printf(""));
-//	printf("test12 - ft_printf: ");
+//	printf("test13 - ft_printf: ");
 //	printf(" (%i)\n",ft_printf(""));
 
-// ------ TEST13 ------
-//	printf("test13 - printf: ");
+// ------ TEST14 ------
+//	printf("test14 - printf: ");
 //	printf(" (%i)\n", printf("%s", 123));
-//	printf("test13 - ft_printf: ");
+//	printf("test14 - ft_printf: ");
 //	printf(" (%i)\n", ft_printf("%s", 123));
 
-// ------ TEST14 ------
-	printf("test14 - printf: ");
+// ------ TEST15 / %u ------
+	printf("test15 - printf: ");
 	printf(" (%i)\n", printf("%u", -123));
-//	printf("test14 - ft_printf: ");
+//	printf("test15 - ft_printf: ");
 //	printf(" (%i)\n", ft_printf("%u", -123));
 
-// ------ TEST15 ------
-//	printf("test15 - printf: ");
+// ------ TEST16 ------
+//	printf("test16 - printf: ");
 //	printf(" (%i)\n", printf("%i", 'A'));
-//	printf("test15 - ft_printf: ");
+//	printf("test16 - ft_printf: ");
 //	printf(" (%i)\n", ft_printf("%i", 'A'));
 
 	return 0;
