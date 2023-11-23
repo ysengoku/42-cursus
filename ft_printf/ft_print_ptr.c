9c52e08f58c4d02531f8f709c81fa96a66034e1c
yusengok <yusengok@student.42.fr>
2023-11-23 11:43:13 +0100

    update ft_print_ptr.c

--- ft_printf/ft_print_ptr.c
+++ ft_printf/ft_print_ptr.c
@@ -6,17 +6,15 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 15:28:26 by yusengok          #+#    #+#             */
-/*   Updated: 2023/11/22 15:30:52 by yusengok         ###   ########.fr       */
+/*   Updated: 2023/11/23 11:40:16 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
 int	ft_print_ptr(void * arg)
-{
-	int	ptr;
-	
-	ptr = &arg;
-	// print in hex
-	
+{	
+	if (!arg)
+		return (-1);
+	return (write(1, "0x", 2) + ft_print_lowerhex(arg));
 }
