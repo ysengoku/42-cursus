2a89191a1ed76a282f719c35e3824952e06abcc2
yusengok <yusengok@student.42.fr>
2023-11-24 14:54:00 +0100

    update ft_print_hex

--- ft_printf/srcs/ft_printf/ft_print_hex.c
+++ ft_printf/srcs/ft_printf/ft_print_hex.c
@@ -6,32 +6,19 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 15:34:50 by yusengok          #+#    #+#             */
-/*   Updated: 2023/11/24 11:38:30 by yusengok         ###   ########.fr       */
+/*   Updated: 2023/11/24 14:52:22 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
 static unsigned int	ft_get_digitcount_hex(int n);
-//static char			*ft_convert_negtobin(int n);
 static char			*ft_itoa_hex(int n, char base);
-
-// version hex de putnbr + counter plustot que itoa + putstr + strlen ?
 
 int	ft_print_hex(int n, char sp)
 {
 	char	*nbr;
 
-	if (n >= 0 && n <= 9)
-	{
-		ft_putchar_fd(n + '0', 1);
-		return (1);
-	}
-	if (n > 9 && n < 16)
-	{
-		ft_putchar_fd('a' + n - 10, 1);
-		return (1);
-	}
 	nbr = ft_itoa_hex(n, sp);
 	ft_putstr_fd(nbr, 1);
 	return (ft_strlen(nbr));
@@ -51,53 +38,104 @@ static unsigned int	ft_get_digitcount_hex(int n)
 		}
 	}
 	else
-		count = 8; // when n < 0, always 8 (convert from 32 bits)
+		count = MAX_BIT / 4; // when n < 0, always 8 (convert from 32 bits)
 	return (count);
 }
 /*
-static char	*ft_convert_negtobin(int n)
-{
-	char			*res;
-	unsigned int	count;
-
-	n = n * (-1) - 1; // get the absolute value & minus 1
-	count = MAX_BIT;
-	res = (char *)ft_calloc(MAX_BIT + 1, sizeof(char));
-	while (count > 0)
-	{
-		res[--count] = (n % 2) + '0';
-		n /= 2;
-	}
-	while (res[count])
-	{
-		if (res[count] == '0')
-			res[count++] = '1';
-		else if (res[count] == '1')
-			res[count++] = '0';
-	}
-	return (res);
-}
+* When the input is a negative int, the conversion is as bellow:
+* 1. Convert the negative int (decimal) to binary
+		binary == 32 bits here
+* 2. Convert it from binary to hex 
+		4 bits == 1 digit in hex
 */
+
 static char	*ft_itoa_hex(int n, char sp)
 {
-	char	*base;
-	char	*nbr;
-	int		digitcount;
+	char			*base;
+	char			*digits;
+	unsigned int	nbr;
+	int				digitcount;
 
+	nbr = n;
 	if (sp == 'x')
 		base = BASE_HEXLOWER;
 	else if (sp == 'X')
 		base = BASE_HEXUPPER;
 	digitcount = ft_get_digitcount_hex(n);
-	nbr = (char *)ft_calloc(digitcount + 1, sizeof(char));
+	digits = (char *)ft_calloc(digitcount + 1, sizeof(char));
+	while (nbr > 0)
+	{
+		digits[--digitcount] = base[nbr % 16];
+		nbr /= 16;
+	}
+	return (digits);
+}
+
+/*
+========== Recursive version (putnbr in hex version) ==========
+#include <stdio.h>
+#include <unistd.h>
+
+void    ft_putchar_fd(char c, int fd)
+{
+    write(fd, &c, 1);
+}
+
+static unsigned int	ft_get_digitcount_hex(int n)
+{
+	unsigned int	count;
+
+	count = 0;
 	if (n > 0)
 	{
 		while (n > 0)
 		{
-			nbr[--digitcount] = base[n % 16];
 			n /= 16;
+			count++;
 		}
 	}
-	// negative number cases
-	return (nbr);
+	else
+		count = 8; // when n < 0, always 8 (convert from 32 bits)
+	return (count);
 }
+
+int    ft_print_hex(int n, char sp)
+{
+    unsigned int    nbr = n;
+//    int count = 0;
+
+    if (nbr > 15)
+    {
+        ft_print_hex(nbr / 16, sp);
+        ft_print_hex(nbr % 16, sp);
+    }
+    else if (nbr >= 0 && nbr <= 9)
+    {
+        ft_putchar_fd(n + '0', 1);
+        count += 1;
+    }
+    else if (nbr > 9 && nbr < 16)
+    {
+        if (sp == 'x')
+            ft_putchar_fd('a' + nbr - 10, 1);
+        else if (sp == 'X')
+            ft_putchar_fd('A' + nbr - 10, 1);
+        count += 1;
+    }
+    return (ft_get_digitcount_hex(n));
+ //   return (count);
+}
+
+int main() {
+    int    arg = 6908;
+    
+    unsigned int    n = arg;
+    printf("Signed int value : %i\n", arg);
+    printf("Unsigned int value : %u\n", n);
+    
+    printf(" (%i)\n", printf("%x", arg));
+    printf(" (%i)\n", ft_print_hex(arg, 'x'));
+    printf(" (%i)\n", ft_print_hex(arg, 'X'));
+    return 0;
+}
+*/
