59d8bfa9bebd2fae9d313645f27d412d9fc48af3
ysengoku <ysengoku.tourisme@gmail.com>
2023-11-25 23:03:30 +0100

    update ft_print_unit.c

--- ft_printf/srcs/ft_print_uint.c
+++ ft_printf/srcs/ft_print_uint.c
@@ -13,16 +13,47 @@
 #include "../includes/ft_printf.h"
 #include "../includes/libft.h"
 
+static int	ft_get_digits_count(long n);
+
 int	ft_print_uint(int n)
 {
-	unsigned long	u_nbr;
-	char			*nbr;
+    unsigned int	nbr;
+    char			*digits;
+	int				count;
 
-	if (n < 0)
-		u_nbr = n + 1 + UINT_MAX;
-	else
-	u_nbr = n;
-	nbr = ft_itoa(u_nbr);
-	ft_putstr_fd(nbr, 1);
-	return (ft_strlen(nbr));
+	nbr = (unsigned int)n;
+	count = ft_get_digits_count((long)nbr);
+	digits = (char *)malloc((count + 1) * sizeof(char));
+	if (!digits)
+		return (-1);
+	digits[count] = '\0';
+	if (nbr == 0)
+		digits[--count] = '0';
+	while (nbr > 0)
+	{
+		digits[--count] = nbr % 10 + '0';
+		nbr /= 10;
+	}
+	ft_putstr_fd(digits, 1);
+	return ((int)ft_strlen(digits));
+}
+
+	static int    ft_get_digits_count(long n)
+{
+    int    count;
+
+    count = 0;
+    if (n < 0)
+    {
+        count += 1;
+        n *= -1;
+    }
+    if (n == 0)
+        count = 1;
+    while (n > 0)
+    {
+        n /= 10;
+        count++;
+    }
+    return (count);
 }
