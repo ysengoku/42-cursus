7b64e2aae5d8535ea0d8f1bcca65c74b829f384b
ysengoku <ysengoku.tourisme@gmail.com>
2023-11-26 22:53:17 +0100

    update test.c & ft_printf.c

--- ft_printf/test.c
+++ ft_printf/test.c
@@ -5,104 +5,104 @@
 
 int	main()
 {
-	printf("-------- TEST / no type specifier --------\n");
+	printf("------------- TEST / no type specifier -------------\n");
 	printf(" (%i)\n", ft_printf("hello"));
 	printf("printf: ");
 	printf(" (%i)\n", printf("hello"));
 
-	printf("\n-------- TEST / c --------\n");
+	printf("\n------------- TEST / c -------------\n");
 	printf(" (%i)\n", ft_printf("%c", 'a'));
 	printf("printf: ");
 	printf(" (%i)\n", printf("%c", 'a'));
 
-	printf("\n-------- TEST / s --------\n");
+	printf("\n------------- TEST / s -------------\n");
 	printf(" (%i)\n", ft_printf("%s", "hello"));
 	printf("printf: ");
 	printf(" (%i)\n", printf("%s", "hello"));
 
-	printf("\n-------- TEST / p --------\n");
+	printf("\n------------- TEST / p -------------\n");
 	const char	*testp = "hello";
 	printf(" (%i)\n", ft_printf("%p", testp));
 	printf("printf: ");
 	printf(" (%i)\n", printf("%p", testp));
 
-	printf("\n-------- TEST / d --------\n");
+	printf("\n------------- TEST / d & i------------\n");
 	printf(" (%i)\n", ft_printf("%d", -42));
 	printf("printf: ");
 	printf(" (%i)\n", printf("%d", -42));	
 
-	printf("\n-------- TEST / i --------\n");
 	printf(" (%i)\n", ft_printf("%i", 0));
 	printf("printf: ");
 	printf(" (%i)\n", printf("%i", 0));
 
-	printf("\n-------- TEST / d --------\n");
 	printf(" (%i)\n", ft_printf("%d", 2147483647));
 	printf("printf: ");
 	printf(" (%i)\n", printf("%d", 2147483647));
 
-	printf("\n-------- TEST / u --------\n");
+	printf("\n------------- TEST / u -------------\n");
 	printf(" (%i)\n", ft_printf("%u", 42));
 	printf("printf: ");
 	printf(" (%i)\n", printf("%u", 42));
 
-	printf("\n-------- TEST / u --------\n");
 	printf(" (%i)\n", ft_printf("%u", -123));
 	printf("printf: ");
 	printf(" (%i)\n", printf("%u", -123));
 
-	printf("\n-------- TEST / x --------\n");
+	printf("\n------------- TEST / x -------------\n");
 	printf(" (%i)\n",ft_printf("%x", 1011));
 	printf("printf: ");
 	printf(" (%i)\n", printf("%x", 1011));
 
-	printf("\n-------- TEST / x --------\n");
 	printf(" (%i)\n",ft_printf("%x", 9));
 	printf("printf: ");
 	printf(" (%i)\n", printf("%x", 9));
 
-	printf("\n-------- TEST / X --------\n");
+	printf("\n------------- TEST / X -------------\n");
 	printf(" (%i)\n",ft_printf("%X", 12));
 	printf("printf: ");
 	printf(" (%i)\n", printf("%X", 12));
 
-	printf("\n-------- TEST / X --------\n");
 	printf(" (%i)\n",ft_printf("%X", -27));
 	printf("printf: ");
 	printf(" (%i)\n", printf("%X", -27));
 
-	printf("\n-------- TEST / %% --------\n");
+	printf("\n------------ TEST / %% -------------\n");
 	printf(" (%i)\n",ft_printf("%%"));
 	printf("printf: ");
 	printf(" (%i)\n", printf("%%"));
 
-	printf("\n-------- TEST / s, d --------\n");
+	printf("\n------------- TEST / s, d -------------\n");
 	printf(" (%i)\n",ft_printf("I'm %s, %d years old.", "Coco", 18));
 	printf("printf: ");
 	printf(" (%i)\n", printf("I'm %s, %d years old.", "Coco", 18));
 
-	printf("\n-------- TEST / empty --------\n");
+	printf("\n------------- TEST / empty -------------\n");
 	printf(" (%i)\n",ft_printf(""));
 	printf("printf: ");
 	printf(" (%i)\n", printf(""));
+
+	printf("\n------------- TEST / invalid specifier -------------\n");
+	printf(" (%i)\n",ft_printf("%a, %s", "Coucou"));
+//	printf("printf: ");
+//	printf(" (%i)\n", printf("%a, %s", "Coucou"));
 
 //	printf("\n-------- TEST / nil arg --------\n");
 //	printf("printf: ");
 //	printf(" (%i)\n", printf("%c"));
 //	printf("ft_printf: ");
-//	printf(" (%i)\n\n",ft_printf(""));
+//	printf(" (%i)\n",ft_printf("%c"));
 
 //	printf("\n-------- TEST / bad type arg --------\n");
 //	printf("printf: ");
 //	printf(" (%i)\n", printf("%s", 123));
 //	printf("ft_printf: ");
-//	printf(" (%i)\n\n", ft_printf("%s", 123));
+//	printf(" (%i)\n", ft_printf("%s", 123));
 
 //	printf("\n-------- TEST / bad type arg --------\n");
 //	printf("printf: ");
 //	printf(" (%i)\n", printf("%i", 'A'));
 //	printf("ft_printf: ");
-//	printf(" (%i)\n\n", ft_printf("%i", 'A'));
+//	printf(" (%i)\n", ft_printf("%i", 'A'));
 
 	return 0;
 }
