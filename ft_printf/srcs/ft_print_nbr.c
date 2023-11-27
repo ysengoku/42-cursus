3442935d463dbb671fda304add37efe2eee25008
yusengok <yusengok@student.42.fr>
2023-11-27 15:34:44 +0100

    update ft_print_nbr

--- ft_printf/srcs/ft_print_nbr.c
+++ ft_printf/srcs/ft_print_nbr.c
@@ -13,10 +13,35 @@
 #include "../includes/ft_printf.h"
 #include "../includes/libft.h"
 
+/*
 int	ft_print_nbr(int n)
 {
 	char	*nbr;
 
 	nbr = ft_itoa(n);
 	return (ft_print_str(nbr));
+}
+*/
+int    ft_print_char(char c)
+{
+    return (write(1, &c, 1));
+}
+
+int    ft_print_nbr(int n)
+{
+	long	nbr;
+	int		count;
+
+	nbr = n;
+	if (n < 0)
+	{
+		return (write(1, "-", 1) + ft_print_nbr(nbr * -1));
+	} // does not work with INT_MIN
+	else if (nbr < 10)
+		ft_print_char(nbr + '0');
+	else
+	{
+		count = ft_print_nbr(nbr / 10);
+		return (count + ft_print_nbr(nbr % 10));
+	}
 }
