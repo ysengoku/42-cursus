a70912a082f83f360d279ce38481ef51e4dcfdc8
yusengok <yusengok@student.42.fr>
2023-11-27 13:05:42 +0100

    update print_char & print_str

--- ft_printf/srcs/ft_print_str.c
+++ ft_printf/srcs/ft_print_str.c
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 08:30:53 by yusengok          #+#    #+#             */
-/*   Updated: 2023/11/27 09:21:29 by yusengok         ###   ########.fr       */
+/*   Updated: 2023/11/27 12:10:25 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,13 @@ int	ft_print_str(char *str)
 	int	count;
 	int	i;
 
-	if (!str)
-		return (-1);
 	count = 0;
 	i = 0;
+	if (!str)
+	{
+		ft_putstr_fd("(null)", 1);
+		return (6);
+	}
 	while (str[i])
 		count += ft_print_char(str[i++]);
 	return (count);
