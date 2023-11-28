f779f51fcd10d8f834f310c6d017c1ebcc1b51eb
yusengok <yusengok@student.42.fr>
2023-11-28 11:06:48 +0100

    add long text test

--- ft_printf/test.c
+++ ft_printf/test.c
@@ -21,6 +21,53 @@ int	main()
 	printf("printf: ");
 	printf(" (%i)\n\n", printf("%s", "hello"));
 
+	const char	*testLongText = "Lorem ipsum dolor sit amet, consectetur \
+	adipiscing elit. Nullam lorem eros, ornare nec aliquam quis, semper eget dolor. \
+	Suspendisse sagittis, ligula nec placerat luctus, eros sem laoreet augue, quis \
+	fringilla leo sem eu quam. Vivamus hendrerit purus tortor, sed pulvinar tortor \
+	commodo quis. In egestas et lorem nec malesuada. Ut efficitur ex nec eros mattis \
+	vulputate. Morbi elit libero, mattis vel pulvinar ac, aliquam ac dolor. \
+	Fusce quis justo at sem vehicula volutpat. Nullam sollicitudin blandit turpis. \
+	Vivamus quis mollis orci. Fusce non purus posuere, placerat tortor sed, efficitur \
+	felis. Aenean vehicula pharetra urna non rhoncus. Duis in arcu libero. Ut vitae \
+	ipsum sit amet ligula dapibus tincidunt. Nullam diam mauris, condimentum eget dui \
+	eu, congue varius leo. Nam blandit venenatis est, a euismod ex convallis sed. \
+	Curabitur id ipsum sollicitudin erat rutrum consequat. Nam cursus dui eu tortor \
+	dapibus fermentum. Vestibulum blandit nibh sed leo varius vulputate. \n \
+	Nulla elementum quis justo a malesuada. Donec elementum elit nec rutrum \
+	vestibulum. Duis bibendum magna vitae ante tempor semper. Suspendisse mattis at \
+	mi non eleifend. Donec ultricies odio dui, eget elementum nunc fermentum non. \
+	Sed id dui ut justo porta finibus ut vitae magna. Mauris malesuada ante at nisi \
+	semper vehicula. Quisque nec orci tincidunt augue venenatis aliquet sed eget risus. \
+	Donec facilisis porttitor lectus et porta. Vestibulum ante ipsum primis in \
+	faucibus orci luctus et ultrices posuere cubilia curae; Etiam non molestie felis, \
+	in ullamcorper sem. Nunc sed sodales dui. Proin lectus massa, varius at mi non, \
+	pulvinar rhoncus urna. Praesent egestas vel tellus sed aliquam. \n Vestibulum eu \
+	commodo velit. Vivamus pulvinar ex a nisi rhoncus, et scelerisque augue blandit. \
+	Duis eget commodo lorem. Cras massa ipsum, dapibus sit amet euismod id, commodo \
+	eu erat. Mauris lobortis vel mi a tempor. Praesent ornare mauris ligula, sit amet \
+	tempus tortor bibendum rhoncus. Sed sed sollicitudin nibh. Pellentesque habitant \
+	morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed sit \
+	amet turpis suscipit, consectetur ante vel, elementum neque. Donec hendrerit est \
+	ut urna rhoncus porttitor. Ut est risus, hendrerit id nunc eu, sodales vehicula \
+	lorem. Etiam dui ex, sodales quis justo sit amet, semper feugiat tellus. \n \
+	Aliquam luctus faucibus dictum. Vivamus finibus erat dui, in maximus justo \
+	pretium at. Nullam sed lectus enim. Fusce eget purus in sapien vestibulum \
+	pulvinar sit amet non metus. Integer quis dolor orci. Etiam lectus mi, sagittis \
+	nec lorem ut, efficitur vulputate enim. Donec maximus quam vel est fringilla \
+	malesuada eget at turpis. Pellentesque tincidunt erat id mollis tempus. Maecenas \
+	dignissim risus varius leo rutrum, at bibendum nisi vulputate. Proin sem tortor, \
+	convallis eu scelerisque non, sodales vitae quam. Duis est risus, scelerisque at \
+	urna vitae, porttitor hendrerit ex. \n Interdum et malesuada fames ac ante \
+	ipsum primis in faucibus. Pellentesque auctor rhoncus dapibus. Quisque viverra \
+	id diam eget condimentum. Mauris sit amet tempor velit, eget mattis ex. Aenean \
+	arcu nunc, ornare quis commodo nec, pretium quis diam. Nam sed libero nec dolor \
+	rhoncus accumsan. Suspendisse sapien eros, viverra et sapien quis, semper \
+	venenatis ex. In ut nisi ex. Orci varius natoque penatibus et magnis dis."; 
+	printf(" (%i)\n", ft_printf("%s", testLongText));
+	printf("printf: ");
+	printf(" (%i)\n\n", printf("%s", testLongText));
+	
 	printf(" (%i)\n", ft_printf(" NULL %s NULL ", (char *)NULL));
 	printf("printf: ");
 	printf(" (%i)\n\n", printf(" NULL %s NULL ", (char *)NULL));
@@ -46,9 +93,9 @@ int	main()
 	printf("printf: ");
 	printf(" (%i)\n\n", printf("%d, %d", -42, -5));	
 
-	printf(" (%i)\n", ft_printf("%i, %i", 0, 7));
+	printf(" (%i)\n", ft_printf("%i, %i, %i", 0, 7, 101));
 	printf("printf: ");
-	printf(" (%i)\n\n", printf("%i, %i", 0, 7));
+	printf(" (%i)\n\n", printf("%i, %i, %i", 0, 7, 101));
 
 	printf(" (%i)\n", ft_printf("%d, %d", INT_MAX, INT_MIN));
 	printf("printf: ");
@@ -71,9 +118,9 @@ int	main()
 	printf("printf: ");
 	printf(" (%i)\n\n", printf("%u", 42));
 
-	printf(" (%i)\n", ft_printf("%u", -123));
+	printf(" (%i)\n", ft_printf("%u, %u", -123, 0));
 	printf("printf: ");
-	printf(" (%i)\n\n", printf("%u", -123));
+	printf(" (%i)\n\n", printf("%u, %u", -123, 0));
 
 	printf(" (%i)\n", ft_printf("%u, %u", INT_MIN, INT_MAX));
 	printf("printf: ");
@@ -93,9 +140,9 @@ int	main()
 	printf(" (%i)\n\n", printf("%x", 0));
 
 	printf("\n------------- TEST / X -------------\n");
-	printf(" (%i)\n",ft_printf("%X", 12));
+	printf(" (%i)\n",ft_printf("%X, %X", 15, 16));
 	printf("printf: ");
-	printf(" (%i)\n\n", printf("%X", 12));
+	printf(" (%i)\n\n", printf("%X, %X", 15, 16));
 
 	printf(" (%i)\n",ft_printf("%X", -27));
 	printf("printf: ");
@@ -120,12 +167,12 @@ int	main()
 	printf("printf: ");
 	printf(" (%i)\n\n", printf(""));
 
-//	printf("\n------------- TEST / invalid specifier -------------\n");
-//	printf(" (%i)\n",ft_printf("%a", "Coucou")); //--> write nothing & return -1 
+	printf("\n------------- TEST / invalid specifier -------------\n");
+	printf(" (%i)\n",ft_printf("%a", "Coucou")); //--> write nothing & return -1 
 //	printf("printf: ");
 //	printf(" (%i)\n\n", printf("%a", "Coucou"));
 
-//	printf(" (%i)\n",ft_printf("%s, %a", "Coucou")); // --> write the 1st arg & return -1 
+	printf(" (%i)\n",ft_printf("%s, %a", "Coucou")); // --> write the 1st arg & return -1 
 //	printf("printf: ");
 //	printf(" (%i)\n\n", printf("%s, %a", "Coucou"));
 
