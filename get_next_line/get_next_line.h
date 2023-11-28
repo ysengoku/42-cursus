437545df08caaaedd7fb8f485abd781f13a49b60
yusengok <ysengoku.tourisme@gmail.com>
2023-11-28 13:20:50 +0100

    add new files get_next_line

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/28 13:07:33 by yusengok          #+#    #+#             */
+/*   Updated: 2023/11/28 13:12:33 by yusengok         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char *get_next_line(int fd);
+
+#endif
