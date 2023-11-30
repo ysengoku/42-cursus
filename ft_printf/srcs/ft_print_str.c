1dc3ed323b00a2e3b516a6c8fe10a53091440336
yusengok <yusengok@student.42.fr>
2023-11-30 10:28:44 +0100

    update makefile & ft_print_str

--- ft_printf/srcs/ft_print_str.c
+++ ft_printf/srcs/ft_print_str.c
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 08:30:53 by yusengok          #+#    #+#             */
-/*   Updated: 2023/11/30 09:29:30 by yusengok         ###   ########.fr       */
+/*   Updated: 2023/11/30 10:14:47 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,6 @@
 
 int	ft_print_str(char *str)
 {
-	int	count;
-
-	count = 0;
 	if (!str)
 		return (write(1, "(null)", 6));
 	return (write(1, str, ft_strlen(str)));
