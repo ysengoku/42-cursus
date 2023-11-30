c5dc0890b80d144c888ed9cb1805aa0fb7c731b5
yusengok <ysengoku.tourisme@gmail.com>
2023-11-30 13:39:30 +0100

    update ft_printf.h

--- ft_printf/includes/ft_printf.h
+++ ft_printf/includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 08:34:37 by yusengok          #+#    #+#             */
-/*   Updated: 2023/11/27 10:14:56 by yusengok         ###   ########.fr       */
+/*   Updated: 2023/11/30 13:20:23 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,25 @@
 # define FT_PRINTF_H
 
 # include <stdarg.h>
-# include "libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
 
 # define SPECIFIERS "cspdiuxX"
 # define MAX_BIT 32
 # define BASE_HEX_LOWER "0123456789abcdef"
 # define BASE_HEX_UPPER "0123456789ABCDEF"
 
-int	ft_printf(const char *format, ...);
-int	ft_print_char(char c);
-int	ft_print_str(char *str);
-int	ft_print_ptr(void *arg);
-int	ft_print_nbr(long n);
-int	ft_print_uint(int n);
-int	ft_print_hex(int n, char sp);
+int		ft_printf(const char *format, ...);
+int		ft_print_char(char c);
+int		ft_print_str(char *str);
+int		ft_print_ptr(void *arg);
+int		ft_print_nbr(long n);
+int		ft_print_uint(int n);
+int		ft_print_hex(int n, char sp);
+
+void	*ft_calloc(size_t nmemb, size_t size);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
 
 #endif
