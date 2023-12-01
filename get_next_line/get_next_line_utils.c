a2309f23cc0a655597c75e03c4d6a08a217aaff9
yusengok <ysengoku.tourisme@gmail.com>
2023-12-01 15:41:37 +0100

    update ft_strchr

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,15 +6,23 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 13:07:25 by yusengok          #+#    #+#             */
-/*   Updated: 2023/11/28 13:08:47 by yusengok         ###   ########.fr       */
+/*   Updated: 2023/12/01 08:37:44 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_strchr(const char *s, char c)
+char	*ft_strchr(const char *s, int c)
 {
-
+	while (s)
+	{
+		if (s == (char)c)
+			return (s);
+		s++;
+	}
+	if (s == (char)c)
+		return (s);
+	return (NULL);
 }
 
 size_t	ft_strlen(const char *s)
