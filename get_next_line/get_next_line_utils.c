d37d5f0c13a3bff61507148b2478b5adb0599a39
ysengoku <ysengoku.tourisme@gmail.com>
2023-12-05 21:52:37 +0100

    add ft_strjoin & ft_substr

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -37,5 +37,47 @@ size_t	ft_strlen(const char *s)
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
+	char			*res;
+	unsigned int	res_len;
+	unsigned int	i;
+	unsigned int	j;
 
+	res_len = ft_strlen((char *)s1) + ft_strlen((char *)s2);
+	res = (char *)malloc(res_len * sizeof(char) + 1);
+	if (!res)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i])
+		res[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		res[j++] = s2[i++];
+	res[j] = '\0';
+	return (res);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substring;
+	size_t	i;
+	size_t	s_len;
+
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen((char *)s);
+	if (start > s_len)
+		return (ft_strdup(""));
+	if (start + len > s_len)
+		len = s_len - start;
+	substring = ft_calloc(len + 1, sizeof(char));
+	if (!substring)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		substring[i] = s[start + i];
+		i++;
+	}
+	return (substring);
 }
