03a1be9de7d9c6f8e111ecb9037263214247c7fc
yusengok <ysengoku.tourisme@gmail.com>
2023-12-11 16:10:39 +0100

    update get_next_line

--- get_next_line/test.c
+++ get_next_line/test.c
@@ -8,10 +8,18 @@ int	main()
 
 	fd = open("test.txt", O_RDONLY);
 //	while ()
-//	printf("%s\n", get_next_line(fd));
+	printf("%s\n", get_next_line(fd));
+
 	close(fd);
 	return 0;
 }
 
 // int open (const char* path, int flags [, int mode ]);
 // returns File Descriptor (small nonnegative int) or -1 (error).
+
+
+/*
+	char buf[BUFSIZE];
+	read(fd, buf, BUFSIZE);
+	printf("%s\n", buf);
+*/
