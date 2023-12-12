bf3738fbadc1272771c7639073a2cc3003df2551
ysengoku <ysengoku.tourisme@gmail.com>
2023-12-12 22:37:49 +0100

    add bonus part

---
+++ get_next_line/get_next_line_bonus.c
@@ -0,0 +1,86 @@
+#include "get_next_line_bonus.h"
+
+static void	ft_initialize_buf(char *buf);
+static char	*ft_store_buf(int fd, char **stash);
+static char	*ft_truncate_line(char *line);
+
+char	*get_next_line(int fd)
+{
+	static char	*stash[MAX_FD];
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &line, 0) < 0)
+	{
+		free(stash[fd]);
+		return (NULL);
+	}
+	line = ft_store_buf(fd, &stash[fd]);
+	if (!line)
+	{
+		free(stash[fd]);
+		return (NULL);
+	}
+	stash[fd] = ft_truncate_line(line);
+	return (line);
+}
+
+static void	ft_initialize_buf(char *buf)
+{
+	unsigned long	i;
+
+	i = 0;
+	while (i < (BUFFER_SIZE + 1) * sizeof(char))
+		buf[i++] = '\0';
+}
+
+static char	*ft_store_buf(int fd, char **stash)
+{
+	char	*buf;
+	char	*tmp;
+	int		read_size;
+
+	buf = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buf)
+		return (NULL);
+	ft_initialize_buf(buf);
+	read_size = 1;
+	while (!ft_strchr(buf, '\n') && read_size > 0)
+	{
+		read_size = read(fd, buf, BUFFER_SIZE);
+		buf[read_size] = '\0';
+		if (read_size == 0)
+			break ;
+		if (stash[fd] == NULL)
+			stash[fd] = ft_strdup("");
+		tmp = stash[fd];
+		stash[fd] = ft_strjoin(tmp, buf);
+		free(tmp);
+	}
+	free(buf);
+	return (stash[fd]);
+}
+
+static char	*ft_truncate_line(char *line_buf)
+{
+	int		i;
+	char	*tmp;
+
+	i = 0;
+	if (line_buf)
+	{
+		while (line_buf[i] != '\n' && line_buf[i] != '\0')
+			i++;
+		if (line_buf[i] == '\0' || line_buf[i + 1] == '\0')
+			return (NULL);
+		tmp = ft_substr(line_buf, i + 1, ft_strlen(line_buf) - i);
+		if (!tmp)
+			{
+				free(line_buf);
+				return (NULL);
+			}
+		line_buf[i + 1] = '\0';
+		return (tmp);
+	}
+	free(line_buf);
+	return (NULL);
+}
---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,22 @@
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 6
+# endif
+
+# ifndef MAX_FD
+#  define MAX_FD 1024
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strdup(const char *s);
+
+#endif
