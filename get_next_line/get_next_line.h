d7020b7d3aa7ce36a3ea947a9f9b20406886d53f
yusengok <ysengoku.tourisme@gmail.com>
2023-12-18 14:33:54 +0100

    update (ok sauf paco strict)

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -26,5 +26,6 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strdup(const char *s);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 
 #endif
